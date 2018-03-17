c3f5d389ad7cd05eaff2ba7d07ff76288cebdb93
stanislasbdx <perso@stan1712.fr>
2018-03-17 17:00:56 +0100

    New bootstrap, new page !

--- index.php
+++ index.php
@@ -1,72 +1,111 @@
 <html lang="fr">
 	<head>
-		<title>Gestion Rob Ellul</title>
+		<title>Rover control station</title>
 		<link rel="stylesheet" href="css/bootstrap.css" media="screen">
 		<link rel="stylesheet" href="css/custom.min.css">
-		<link rel="stylesheet" href="css/style_perso.css">
 		<meta charset="UTF-8">
 	</head>
 	<body>
-		<u><h3 href="index.php" class="text-center">Gestion du Rob Ellul</h3></u><hr />
-			<br>
-			<div class="panel panel-success col-lg-7">
-			  <div class="panel-heading">
-				<h3 class="panel-title">Panel vitesse</h3> 
-			  </div>
-			  <div class="panel-body">
-				<p>Vitesse : <div class="success">50%</div></p>
-			  </div>
-			</div>
+		<?php
+			include('config.php');
+			include('scripts/states.php');
+		?>
+		<h3 class="text-center"><?php echo $nickname; ?> control station</h3><hr />
+		<br>
+		<div class="card text-white bg-info mb-3">
+			<div class="card-header">Control panel</div>
+			<div class="card-body">
+				<h4 class="card-title">Cockpit</h4>
+				<p><?php
+					$autopilot = "true";
 
-			<div class="panel panel-danger col-lg-7">
-			  <div class="panel-heading">
-				<h3 class="panel-title">Panel de contrôle</h3>
-			  </div>
-			  <div class="panel-body">
-				<p>Pilote automatique : <div class="error">INCONNU</div></p>
-				<button href="#" target="_blank" id="little_up_left">&#x2196;</button>
-				<button href="#" target="_blank" id="up">&#x2191;</button>
-				<button href="#" target="_blank" id="little_up_right">&#x2197;</button>
-				<br />
-				<button href="#" target="_blank" id="left">&#x2190;</button>
-				<button href="#" target="_blank" id="stop">&#x2715;</button>
-				<button href="#" target="_blank" id="right">&#x2192;</button>
-				<br />
-				<button href="#" target="_blank" id="little_down_left">&#x2199;</button>
-				<button href="#" target="_blank" id="down">&#x2193;</button>
-				<button href="#" target="_blank" id="little_down_right">&#x2198;</button>
-				<a href="javascript:window.open('img/pdc.png','popup','width=500,height=500,left=50,top=50,scrollbars=0')">Pad Num&#xE9;rique</a><!-- https://placeholdit.imgix.net/~text?txtsize=33&txt=%E2%98%A3%F0%9F%98%8EPad%20de%20Contr%C3%B4le%F0%9F%98%8E%E2%98%A3&w=500&h=500 -->
-			  </div>
-			</div>
+					if($autopilot == "true") {
+					echo "The discovery mode is activated.";
+					}
+					elseif($autopilot == "false") {
+					echo "The discovery mode is deactivated.";
+					}
+					else{
+					echo "The discorvery mode is unavailable or missing, please take manual controls";
+					}
+				?></p>
 
-			<div class="panel panel-success col-lg-7">
-			  <div class="panel-heading">
-				<h3 class="panel-title">Panel d'etat</h3>
-			  </div>
-			  <div class="panel-body">
-				<p>Etat : <div class="error">INCONNU</div></p>
-				<p>Batterie : 
-					<div class="alert alert-dismissible alert-danger">
-					  <button type="button" class="close" data-dismiss="alert">&times;</button>
-					  <strong>ATTENTION !</strong> Batterie faible !
+				<ul class="nav nav-tabs">
+					<li class="nav-item">
+						<a class="nav-link active" data-toggle="tab" href="#autopilot">Discovery mode / Auto pilot</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" data-toggle="tab" href="#manual">Manual mode</a>
+					</li>
+				</ul>
+				<div id="myTabContent" class="tab-content">
+					<div class="tab-pane fade show active" id="autopilot">
+						<p>Into the void</p>
 					</div>
-					<div class="progress progress-striped active">
-					  <div class="progress-bar progress-bar-success" style="width: 75%">75%</div>
+					<div class="tab-pane fade" id="manual">
+						<div class="panel-body">
+							<button href="#" target="_blank" id="little_up_left">&#x2196;</button>
+							<button href="#" target="_blank" id="up">&#x2191;</button>
+							<button href="#" target="_blank" id="little_up_right">&#x2197;</button>
+							<br />
+							<button href="#" target="_blank" id="left">&#x2190;</button>
+							<button href="#" target="_blank" id="stop">&#x2715;</button>
+							<button href="#" target="_blank" id="right">&#x2192;</button>
+							<br />
+							<button href="#" target="_blank" id="little_down_left">&#x2199;</button>
+							<button href="#" target="_blank" id="down">&#x2193;</button>
+							<button href="#" target="_blank" id="little_down_right">&#x2198;</button>
+						</div>
 					</div>
-				</p>
-			  </div>
+				</div>
 			</div>
-			<div class="panel panel-default alignright">
-			<p>Etat camera : <div class="warning">INCONNUE</div></p>
-			<table border=2> 
-				<tr>
-				<th><img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=CAMERA&w=400&h=400" /></th> 
-				</tr>
-			</table>
+		</div>
+
+		<div class="card text-white bg-info mb-3">
+			<div class="card-header"><?php echo $nickname; ?>'s ETA</div>
+			<div class="card-body">
+
+				<h4 class="card-title">State : </h4><p><?php
+				if ($echo == 0) {
+					echo "N/A";
+				}
+				elseif ($echo == 1) {
+					echo "Available";
+				}
+				elseif ($echo == 2) {
+					echo "Unavailable";
+				}
+				else {
+					echo "N/A";
+				}
+				?></p>
+				<h4 class="card-title">Speed :</h4>
+				<p><?php echo $speed," m/s"; ?></p>
+
+				<h4 class="card-title">Battery :</h4>
+				<?php
+					if ($battery < 0) {
+						include('include/errorprogressbar.php');
+					}
+					elseif ($battery < 15) {
+						include('include/15progressbar.php');
+					}
+					elseif ($battery < 30) {
+						include('include/30progressbar.php');
+					}
+					elseif ($battery <= 100) {
+						include('include/100progressbar.php');
+					}
+					elseif ($battery > 100) {
+						include('include/errorprogressbar.php');
+					}
+				?>
+			</div>
 		</div>
 	<body>
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
-  <script type="text/javascript">/* <![CDATA[ */(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */</script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript">/* <![CDATA[ 							*/(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */
+	</script>
 </html>
