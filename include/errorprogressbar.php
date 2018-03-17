ce82b78610f8b5420ea18bedf9d25b153b606d11
stanislasbdx <perso@stan1712.fr>
2018-03-17 17:00:41 +0100

    Forgot this file :/ progressbar behavior

---
+++ include/errorprogressbar.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger">
+  <strong>WARNING !</strong> <?php echo $nickname; ?>'s battery is illogical, his battery can't be under 0 or higher than 100 !
+  <br />There is the value that the system gives to me <?php echo $battery; ?>
+</div>
